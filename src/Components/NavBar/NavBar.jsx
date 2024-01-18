aee610720ab88da9711721cc9da86e09f34d3ea5
Mahmoud <mhmoodsh1996@gmail.com>
2024-01-19 00:02:29 +0300

    second commit

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,7 +3,7 @@ function NavBar() {
     <>
       <section className="Logo">Here comes the logo</section>
       <section className="navigations">Here comes the NAVIGATIONS</section>
-      <section className="get-started"></section>
+      <section className="get-started">HERE COMES THE GET STARTED BUTTON  </section>
     </>
   );
 }
