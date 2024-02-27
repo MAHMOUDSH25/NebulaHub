a93ec0ab8bd71606173a30d0342f9372fb9b46b7
Mahmoud <mhmoodsh1996@gmail.com>
2024-02-27 12:12:40 +0300

    full upload after copying the new code

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,10 +4,10 @@ function Hero() {
   return (
     <>
       <div className="hero">
-        <section className="hero__section">
+        <section className="hero__section container">
           <h1 id="header">
-            Empower content, Unleash possibilities <br /> with ultimate headless
-            CMS
+            Empower content, Unleash possibilities <br /> with ultimate <text id="purple__text">headless
+            CMS</text>
           </h1>
           <p id="paragraph">
             Craft, Manage, an deliver content like never before - welcome to a
@@ -16,7 +16,7 @@ function Hero() {
           </p>
           <div className="hero__btn">
             <button className="btn" id="start__btn">Get Started</button>
-            <button className="btn" id="create__btn">create headless</button>
+            <button className="btn" id="create__btn">Create Headless for your project</button>
           </div>
         </section>
       </div>
@@ -25,3 +25,45 @@ function Hero() {
 }
 
 export default Hero;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import "./Hero.scss";
+
+// function Hero() {
+//   return (
+//     <>
+//       <div className="hero">
+//         <section className="hero__section">
+//           <h1 id="header">
+//             Empower content, Unleash possibilities <br /> with ultimate headless
+//             CMS
+//           </h1>
+//           <p id="paragraph">
+//             Craft, Manage, an deliver content like never before - welcome to a
+//             headless CMS <br /> experience that redefines agility and
+//             flexability{" "}
+//           </p>
+//           <div className="hero__btn">
+//             <button className="btn" id="start__btn">Get Started</button>
+//             <button className="btn" id="create__btn">create headless</button>
+//           </div>
+//         </section>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Hero;
